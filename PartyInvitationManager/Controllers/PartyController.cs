d70eeeac8c72dced7bee842cdf9cf9b704324608
jerinpious <jpious4925@conestogac.on.ca>
2025-03-09 19:41:23 -0400

    Create, Edit, Delete Party Controller and Views

---
+++ PartyInvitationManager/Controllers/PartyController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PartyInvitationManager.Models;
+
+namespace PartyInvitationManager.Controllers
+{
+    public class PartyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PartyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Party
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Parties.ToListAsync());
+        }
+
+        // GET: Party/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var party = await _context.Parties.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (party == null)
+                return NotFound();
+
+            return View(party);
+        }
+
+        // GET: Party/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Party/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Date,Location")] Party party)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(party);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(party);
+        }
+
+        // GET: Party/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var party = await _context.Parties.FindAsync(id);
+            if (party == null)
+                return NotFound();
+
+            return View(party);
+        }
+
+        // POST: Party/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Date,Location")] Party party)
+        {
+            if (id != party.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(party);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Parties.Any(e => e.Id == id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(party);
+        }
+
+        // GET: Party/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var party = await _context.Parties.FirstOrDefaultAsync(m => m.Id == id);
+            if (party == null)
+                return NotFound();
+
+            return View(party);
+        }
+
+        // POST: Party/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var party = await _context.Parties.FindAsync(id);
+            if (party != null)
+            {
+                _context.Parties.Remove(party);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
